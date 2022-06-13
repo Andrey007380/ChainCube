ce6996dcddfbf9309758fce259a5d96b348d0300
Andrey007380 <andrey.lishtva@gmail.com>
2022-06-13 22:05:51 +0300

    Linedender fix

--- Assets/Scripts/TileCubeMover.cs
+++ Assets/Scripts/TileCubeMover.cs
@@ -7,7 +7,8 @@ public class TileCubeMover : MonoBehaviour, IMovable
 {
     private Rigidbody _rigidbody;
     private Transform _transform;
-
+    
+    Color c2 = new Color(1, 1, 1, 0.5f);
     private LineRenderer lineRenderer;
     
     private float _movementSpeed = 25f;
@@ -22,6 +23,9 @@ public class TileCubeMover : MonoBehaviour, IMovable
     private void Awake()
     {
         lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
+        lineRenderer.startColor = c2;
+        lineRenderer.endColor = c2;
         _rigidbody = GetComponent<Rigidbody>();
         _camera = Camera.main;
     }
@@ -42,15 +46,18 @@ public class TileCubeMover : MonoBehaviour, IMovable
         _ray = _camera.ScreenPointToRay(Input.mousePosition);
         if (!Physics.Raycast(_ray, out _hit)) 
             return;
-
+        if (!(_rigidbody.position.x > -2) || !(_rigidbody.position.x < 2)) 
+            return;
         var position = _rigidbody.position;
         _rigidbody.position = Vector3.Lerp(position, new Vector3(_hit.point.x,position.y,position.z), Time.deltaTime * _movementSpeed);
-        
-        Vector3[] positions = new Vector3[3];
-        positions[0] = transform.position;
-        positions[1] = transform.forward * 20 + transform.position;
+                    
+        Vector3[] positions = new Vector3[2];
+        positions[0] = transform.forward * 10;
         lineRenderer.positionCount = positions.Length;
         lineRenderer.SetPositions(positions);
+            
+        lineRenderer.alignment = LineAlignment.View;
+
     }
 
     #endregion
